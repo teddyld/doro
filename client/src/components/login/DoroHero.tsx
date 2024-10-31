2a5c891178a969fc99b3df705540704b1c3f3717
Vincent <vincent.pham2@outlook.com>
2024-11-01 00:54:23 +1100

    Update DoroHero usability

--- client/src/components/login/DoroHero.tsx
+++ client/src/components/login/DoroHero.tsx
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function DoroHero() {
   return (
-    <div
-      className="flex cursor-pointer flex-col items-center justify-center text-4xl"
-      aria-label="Link to dashboard"
-    >
-      <h1>
-        <Link to="/">
-          Pomo<span className="text-primary">doro</span>
-        </Link>
+    <Link to="/" className="mx-auto">
+      <h1
+        aria-label="link-dashboard"
+        className="cursor-pointer justify-center text-4xl"
+      >
+        Pomo<span className="text-primary">doro</span>
       </h1>
-    </div>
+    </Link>
   );
 }
