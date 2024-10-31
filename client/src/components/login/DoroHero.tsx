9d4b4d749dce3cb38c7da01026c6cecfda69a033
Vincent <vincent.pham2@outlook.com>
2024-11-01 01:07:44 +1100

    Update aria-label on DoroHero

--- client/src/components/login/DoroHero.tsx
+++ client/src/components/login/DoroHero.tsx
@@ -2,11 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function DoroHero() {
   return (
-    <Link to="/" className="mx-auto">
-      <h1
-        aria-label="link-dashboard"
-        className="cursor-pointer justify-center text-4xl"
-      >
+    <Link to="/" className="mx-auto" aria-label="link-dashboard">
+      <h1 className="cursor-pointer justify-center text-4xl">
         Pomo<span className="text-primary">doro</span>
       </h1>
     </Link>
